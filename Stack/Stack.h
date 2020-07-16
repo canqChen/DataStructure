d1fbc0b7e15216ccd90569879cdb3a9779066413
canq.chen <canq.chen@gmail.com>
2020-07-16 12:52:55 +0800

    complete the class of sequential stack

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -6,142 +6,60 @@
 
 using namespace std;
 
-class Stack
+class SeqStack
 {
 public:
-	Stack()
+	SeqStack(int maxSize = MAXSIZE)
 	{
-		stack = new ElemType[MAXSIZE];
+		stack = new ElemType[maxSize];
 
 		if (stack == nullptr)
 		{
 			throw string("fail to allocate memory.");
 		}
-
-		length = 0;
-		top = -1;
+		this->maxSize = maxSize;
+		size = 0;
+		top = 0;
 	}
 
-	void insert(ElemType ele, int pos = -1)
+	void push(ElemType ele)
 	{
 		if (stack == nullptr)
-		{
-			cout << "the stack is not initialized!";
-			return;
-		}
-		else if (length == MAXSIZE)
-		{
-			cout << "fail to insert new element into the stack, the stack is full!";
-			return;
-		}
-		//insert
-		if (pos == -1)  //default, insert into the tail
-		{
-			stack[length++] = ele;
-		}
-		else if (pos<1 || pos>length + 1)  //invalid position
-		{
-			cout << "fail to insert new element into the position of " << pos;
-			return;
-		}
-		else   //valid position, insert into the middle (head)
-		{
-			for (int i = length; i >= pos; i--)
-			{
-				stack[i] = stack[i - 1];
-			}
-
-			stack[pos - 1] = ele;
-			length++;
-		}
+			throw string("the stack is not initialized!");
+		else if(size==maxSize)
+			throw string("Stack overflow!");
+			
+		stack[top++] = ele;
+		size++;
 	}
 
-	void remove(int pos)
+	ElemType pop()
 	{
 		if (is_empty())
 		{
-			cout << "the stack is empty!";
-			return;
+			throw string("Stack underflow!");
 		}
-		else
-		{
-			if (pos < 1 || pos >= length + 1)  //invalid position
-			{
-				cout << "invalid position of " << pos;
-				return;
-			}
-			for (int i = pos - 1; i < length - 1; i++)
-			{
-				stack[i] = stack[i + 1];
-			}
-			length--;
-		}
+		size--;
+		return stack[--top];
 	}
 
-	void remove_first(ElemType ele)
+	ElemType getTop()
 	{
 		if (is_empty())
 		{
-			cout << "the stack is empty!";
-			return;
+			throw string("the stack is empty!");
 		}
-		else
-		{
-			for (int i = 0; i < length; i++)
-			{
-				if (stack[i] == ele)
-				{
-					if (i != length - 1)  // remove the element in the middle, the last one just reduce the length
-					{
-						for (int j = i; j < length; j++)
-						{
-							stack[j] = stack[j + 1];
-						}
-						length--;
-					}
-					break;
-				}
-			}
-		}
+		return stack[top-1];
 	}
 
-	void remove_all(ElemType ele)
-	{
-		if (is_empty())
-		{
-			cout << "the stack is empty!";
-		}
-		else
-		{
-			for (int i = 0; i < length; i++)
-			{
-				if (stack[i] == ele)
-				{
-					for (int j = i; j < length - 1; j++)
-					{
-						stack[j] = stack[j + 1];
-					}
-					length--;
-				}
-			}
-		}
-	}
-
-	void print()
-	{
-		for (int i = 0; i < length; i++)
-		{
-			cout << stack[i] << "    ";
-		}
-	}
-	
 	void destroy()
 	{
 		if (stack != nullptr)
 		{
 			delete[] stack;
 			stack = nullptr;
-			length = 0;
+			size = 0;
+			top = 0;
 		}
 	}
 
@@ -149,22 +67,24 @@ public:
 	{
 		if (stack != nullptr)
 		{
-			length = 0;
+			size = 0;
+			top = 0;
 		}
 	}
 
-	~Stack() {
+	~SeqStack() {
 		if (stack != nullptr)
 			delete[] stack;
 	}
 
 private:
 	ElemType* stack;
-	int length;
+	int size;
 	int top;
+	int maxSize;
 
 	bool is_empty()
 	{
-		return length == 0 ? true : false;
+		return size == 0 ? true : false;
 	}
 };
