c57a7bdb9742c1a888f1fdf092c559d3e5d278e5
canq.chen <canq.chen@gmail.com>
2020-12-26 17:11:17 +0800

    fix the problem of deletion

--- Tree/AVLTree.cpp
+++ Tree/AVLTree.cpp
@@ -24,11 +24,11 @@ void AVLTree<T>::clear()
 		if (tmp != nullptr)
 		{
 			nodeStk.push(tmp);
-			tmp = tmp->getLeft();
+			tmp = tmp->left;
 		}
 		else
 		{
-			tmp = nodeStk.top()->getRight();
+			tmp = nodeStk.top()->right;
 			delete nodeStk.top();
 			nodeStk.pop();
 		}
@@ -48,16 +48,16 @@ void AVLTree<T>::insert(const T& ele)
 	while (tmp != nullptr)
 	{
 		preNode = tmp;
-		if (tmp->getVal() > ele)
-			tmp = tmp->getLeft();
-		else if (tmp->getVal() < ele)
-			tmp = tmp->getRight();
+		if (tmp->value > ele)
+			tmp = tmp->left;
+		else if (tmp->value < ele)
+			tmp = tmp->right;
 		else // already exists, do nothing
 			return;
 	}
 	pTreeNode newNode = new TreeNode<T>(ele);
-	if (preNode->getVal() > ele)
-		preNode->setLeft(newNode);
+	if (preNode->value > ele)
+		preNode->left = newNode;
 	else if (preNode->right < ele)
 		pTreeNode newNode = new TreeNode<T>(ele);
 }
@@ -74,13 +74,13 @@ void AVLTree<T>::erase(const T& ele)
 	// find node to be erased
 	while (tmp != nullptr)
 	{
-		if (tmp->getVal() == ele)
+		if (tmp->value == ele)
 			break;
 		preNode = tmp;
-		if (tmp->getVal() > ele)
-			tmp = tmp->getLeft();
-		else if (tmp->getVal() < ele)
-			tmp = tmp->getRight();
+		if (tmp->value > ele)
+			tmp = tmp->left;
+		else if (tmp->value < ele)
+			tmp = tmp->right;
 	}
 	if (tmp == nullptr)   // not found
 	{
@@ -99,24 +99,24 @@ void AVLTree<T>::deleteByMerging(pTreeNode & node, pTreeNode & parent)
 	pTreeNode tmp = node, newNode = nullptr;
 
 	// leaf node or only one child
-	if (node->getLeft() == nullptr)
-		newNode = node->getRight();
-	else if (node->getRight() == nullptr)
-		newNode = node->getLeft();
+	if (node->left == nullptr)
+		newNode = node->right;
+	else if (node->right == nullptr)
+		newNode = node->left;
 	else  // two children
 	{
-		tmp = node->getLeft();   // cannot be nullptr
-		while (tmp->getRight() != nullptr)
-			tmp = tmp->getRight();
-		tmp->setRight(node->getRight());
+		tmp = node->left;   // cannot be nullptr
+		while (tmp->right != nullptr)
+			tmp = tmp->right;
+		tmp->right = node->right;
 		tmp = node;
-		newNode = node->getLeft();
+		newNode = node->left;
 	}
 
-	if (parent->getLeft() == node)  // the node to be removed is the left child of parent
-		parent->setLeft(newNode);
+	if (parent->left == node)  // the node to be removed is the left child of parent
+		parent->left = newNode;
 	else		//  the node to be removed is the right child of parent
-		parent->setRight(newNode);
+		parent->right = newNode;
 
 	delete tmp;
 }
@@ -127,33 +127,33 @@ void AVLTree<T>::deleteByCopying(pTreeNode & node)
 	pTreeNode tmp = node, prevNode = node, newNode = nullptr;
 
 	// leaf node or only one child, directly replace it
-	if (node->getLeft() == nullptr)
+	if (node->left == nullptr)
 	{
-		if (parent->getLeft() == node)  // the node to be removed is the left child of parent
-			parent->setLeft(node->getRight());
+		if (parent->left == node)  // the node to be removed is the left child of parent
+			parent->left = node->right;
 		else		//  the node to be removed is the right child of parent
-			parent->setRight(node->getRight());
+			parent->right =  node->right;
 	}
-	else if (node->getRight() == nullptr)
+	else if (node->right == nullptr)
 	{
-		if (parent->getLeft() == node)  // the node to be removed is the left child of parent
-			parent->setLeft(node->getLeft());
+		if (parent->left == node)  // the node to be removed is the left child of parent
+			parent->left = node->left;
 		else		//  the node to be removed is the right child of parent
-			parent->setRight(node->getLeft());
+			parent->right = node->left;
 	}
 	else  // two children, copy by precursor node
 	{
-		tmp = node->getLeft();  // cannot be nullptr
-		while (tmp->getRight() != nullptr)  // use the previous node to replace it
+		tmp = node->left;  // cannot be nullptr
+		while (tmp->right != nullptr)  // use the previous node to replace it
 		{
 			prevNode = tmp;
-			tmp = tmp->getRight();
+			tmp = tmp->right;
 		}
-		node->setVal(tmp->getVal());
+		node->value = tmp->value;
 		if (prevNode == node)
-			node->setLeft(tmp->getLeft());
+			node->left  = tmp->left;
 		else
-			prevNode->setRight(tmp->getLeft());
+			prevNode->right  = tmp->left;
 	}
 	delete tmp;
 }
@@ -175,10 +175,10 @@ bool AVLTree<T>::search(const T& ele)
 	pTreeNode tmp = root;
 	while (tmp != nullptr)
 	{
-		if (tmp->getVal() < ele)
-			tmp = tmp->getRight();
-		else if (tmp->getVal() > ele)
-			tmp = tmp->getLeft();
+		if (tmp->value < ele)
+			tmp = tmp->right;
+		else if (tmp->value > ele)
+			tmp = tmp->left;
 		else
 			return true;
 	}
@@ -195,8 +195,8 @@ vector<T>& AVLTree<T>::preOrderTraverse(const pTreeNode & node) const
 	//else
 	//{
 	//	ret.push_back(node->data);
-	//	preOrderTraverse(node->getLeft());
-	//	preOrderTraverse(node->getRight());
+	//	preOrderTraverse(node->left);
+	//	preOrderTraverse(node->right);
 	//}
 	//return ret;
 
@@ -208,12 +208,12 @@ vector<T>& AVLTree<T>::preOrderTraverse(const pTreeNode & node) const
 	{
 		while (tmp != nullptr)
 		{
-			ret.push_back(nodeStk.top()->getVal());
+			ret.push_back(nodeStk.top()->value);
 			nodeStk.push(tmp);
-			tmp = tmp->getLeft();
+			tmp = tmp->left;
 		}
 
-		tmp = nodeStk.top()->getRight();
+		tmp = nodeStk.top()->right;
 		nodeStk.pop();
 	}
 	return ret;
@@ -228,9 +228,9 @@ vector<T>& AVLTree<T>::midOrderTraverse(const pTreeNode & node) const
 	//	return;
 	//else
 	//{
-	//	preOrderTraverse(node->getLeft());
+	//	preOrderTraverse(node->left);
 	//	ret.push_back(node->data);
-	//	preOrderTraverse(node->getRight());
+	//	preOrderTraverse(node->right);
 	//}
 	//return ret;
 
@@ -243,11 +243,11 @@ vector<T>& AVLTree<T>::midOrderTraverse(const pTreeNode & node) const
 		while (tmp != nullptr)
 		{
 			nodeStk.push(tmp);
-			tmp = tmp->getLeft();
+			tmp = tmp->left;
 		}
 
-		ret.push_back(nodeStk.top()->getVal());
-		tmp = nodeStk.top()->getRight();
+		ret.push_back(nodeStk.top()->value);
+		tmp = nodeStk.top()->right;
 		nodeStk.pop();
 	}
 	return ret;
@@ -262,8 +262,8 @@ vector<T>& AVLTree<T>::postOrderTraverse(const pTreeNode &node) const
 	//	return;
 	//else
 	//{
-	//	preOrderTraverse(node->getLeft());
-	//	preOrderTraverse(node->getRight());
+	//	preOrderTraverse(node->left);
+	//	preOrderTraverse(node->right);
 	//	ret.push_back(node->data);
 	//}
 	//return ret;
@@ -276,12 +276,12 @@ vector<T>& AVLTree<T>::postOrderTraverse(const pTreeNode &node) const
 	{
 		while (tmp != nullptr)
 		{
-			ret.push_back(nodeStk.top()->getVal());
+			ret.push_back(nodeStk.top()->value);
 			nodeStk.push(tmp);
-			tmp = tmp->getRight();
+			tmp = tmp->right;
 		}
 
-		tmp = tmp->getLeft();
+		tmp = tmp->left;
 		nodeStk.pop();
 	}
 	reverse(ret.begin(), ret.end());
@@ -298,11 +298,11 @@ vector<T>& AVLTree<T>::levelOrderTraverse(const pTreeNode & node)  const
 	nodeQ.push(node);
 	while (!nodeQ.empty())
 	{
-		ret.push_back(nodeQ.front()->getVal());
-		if (nodeQ.front()->getLeft() != nullptr)
-			nodeQ.push(nodeQ.front()->getLeft());
-		if (nodeQ.front()->getRight() != nullptr)
-			nodeQ.push(nodeQ.front()->getRight());
+		ret.push_back(nodeQ.front()->value);
+		if (nodeQ.front()->left != nullptr)
+			nodeQ.push(nodeQ.front()->left);
+		if (nodeQ.front()->right != nullptr)
+			nodeQ.push(nodeQ.front()->right);
 		nodeQ.pop();
 	}
 }
@@ -314,9 +314,9 @@ AVLTree<T>::pTreeNode AVLTree<T>::copy(const pTreeNode & node)
 		return nullptr;
 	else
 	{
-		pTreeNode newNode = createNode(node->getVal(), node->getBalancedFactor());
-		newNode->setLeft(copy(node->getLeft()));
-		newNode->setRight(copy(node->getRight()));
+		pTreeNode newNode = createNode(node->value, node->BF);
+		newNode->left = copy(node->left);
+		newNode->right = copy(node->right);
 		return newNode;
 	}
 }
@@ -334,8 +334,8 @@ AVLTree<T>::size_type AVLTree<T>::calDepth(const pTreeNode &node)  const
 		return 0;
 	else
 	{
-		size_type lDepth = calDepth(node->getLeft());
-		size_type rDepth = calDepth(node->getRight());
+		size_type lDepth = calDepth(node->left);
+		size_type rDepth = calDepth(node->right);
 		return max(lDepth, rDepth) + 1;
 	}
 }
@@ -352,7 +352,7 @@ AVLTree<T>::size_type AVLTree<T>::calNodeNum(const pTreeNode &node)  const
 	if (node == nullptr)
 		return 0;
 	else
-		return calNodeNum(node->getLeft()) + calNodeNum(node->getRight()) + 1;
+		return calNodeNum(node->left) + calNodeNum(node->right) + 1;
 }
 
 template<typename T>
@@ -366,10 +366,10 @@ AVLTree<T>::size_type AVLTree<T>::calLeafNum(const pTreeNode & node)  const
 {
 	if (node == nullptr)
 		return 0;
-	if (node->getLeft() == nullptr && node->getRight() == nullptr)
+	if (node->left == nullptr && node->right == nullptr)
 		return 1;
 	else
-		return calLeafNum(node->getLeft()) + calLeafNum(node->getRight());
+		return calLeafNum(node->left) + calLeafNum(node->right);
 }
 
 template<typename T>
--- Tree/AVLTree.h
+++ Tree/AVLTree.h
@@ -16,6 +16,7 @@ class AVLTree
 {
 public:
 	typedef T value_type;
+	typedef T& reference;
 	typedef vector<T>::size_type size_type;
 	
 
@@ -39,27 +40,17 @@ public:
 	vector<T>& postOrderTraverse() const { return postOrderTraverse(root); }
 	vector<T>& levelOrderTraverse() const { return levelOrderTraverse(root); }
 
-	
 private:
 	template<typename _T>
 	struct TreeNode
 	{
-	public:
 		typedef TreeNode<_T>* pointer;
 		TreeNode() : value(_T()), BF(0), left(nullptr), right(nullptr) {};
 		TreeNode(const _T & val, int BF = 0, pointer left = nullptr, pointer right = nullptr)
 			: value(val), BF(0), left(left), right(right) {};
-		void setVal(const _T& val) { this->value = val; }
-		const _T& getVal() const { return this->value; }
-		void setBalancedFactor(const int& bf) { this->BF = bf; }
-		const int& getBalancedFactor() const { return this->BF; }
-		void setLeft(const pointer & node) { this->left = node; }
-		void setRight(const pointer & node) { this->right = node; }
-		const pointer& getLeft() const { return this->left; }
-		const pointer& getRight() const { return this->right; }
-	private:
+		
 		_T value;
-		TreeNode<_T>* left, *right,  *parent;
+		pointer left, *right,  *parent;
 		int BF;    // left height - right height
 	};
 	typedef TreeNode<T>* pTreeNode;
@@ -76,7 +67,7 @@ private:
 	size_type calNodeNum(const pTreeNode &node) const;
 	size_type calLeafNum(const pTreeNode & node) const;
 
-	pTreeNode& createNode(const T& ele, int BF = 0, pTreeNode left = nullptr, pTreeNode right = nullptr)
+	pTreeNode& createNode(const reference ele, int BF = 0, pTreeNode left = nullptr, pTreeNode right = nullptr)
 	{ return new TreeNode<T>(ele, BF, left, right); }
 
 	void deleteByMerging(pTreeNode & node, pTreeNode & parent);
